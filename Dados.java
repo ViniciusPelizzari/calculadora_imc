2932448d868d60f83bfa1a332a4b704ebce04c57
ViniciusPelizzari <vinipelizzari2410@gmail.com>
2023-02-15 09:44:20 -0300

    Versão 1.1 - apresentação na tela

--- Dados.java
+++ Dados.java
@@ -1,42 +1,51 @@
 
-//importando método Scanner para leitura de dados
-import java.util.Scanner;
+//importando método para entrada e saída de dados
+import javax.swing.JOptionPane;
 
 /**
  * @author Vinícius Pelizzari
  */
 public class Dados {
     public static void main(String[] args) {
-        // criando objeto
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Digite a sua altura em metros (ex: 1,80): ");
-        // entrada de dados
-        float altura = scanner.nextFloat();
-        System.out.print("Digite seu peso (ex: 79,70) ");
-        // entrada de dados
-        float peso = scanner.nextFloat();
+        // forçando saída de dados imediatamente
+        System.out.flush();
+
+        /**
+         * apresentando tela
+         * entrada de dados
+         * saída de dados
+         */
+        String alt = JOptionPane.showInputDialog(null, "Digite a sua altura em metros (ex: 1.80): ");
+        float altura = Float.parseFloat(alt);
+        String kg = JOptionPane.showInputDialog(null, "Digite seu peso (ex: 79.70): ");
+        float peso = Float.parseFloat(kg);
 
         // cálculo do imc
         float imc = peso / (altura * altura);
+
         // forçando saída de dados imediatamente
         System.out.flush();
 
         // condições e apresentação na tela
         if (imc <= 18.4) {
-            System.out.println("Seu IMC é de:" + imc);
-            System.out.println("Você é classificado como 'MAGRO', com grau de obesidade 0\n");
+            JOptionPane.showMessageDialog(null, "Você é classificado como 'MAGRO', com grau de obesidade 0",
+                    "Seu IMC é de: " + imc, JOptionPane.WARNING_MESSAGE);
+
         } else if (imc >= 18.5 && imc <= 24.9) {
-            System.out.println("Seu IMC é de:" + imc);
-            System.out.println("Você é classificado como 'NORMAL', com grau de obesidade 0\n");
+            JOptionPane.showMessageDialog(null, "Você é classificado como 'NORMAL', com grau de obesidade 0",
+                    "Seu IMC é de: " + imc, JOptionPane.WARNING_MESSAGE);
+
         } else if (imc >= 25.0 && imc <= 29.9) {
-            System.out.println("Seu IMC é de:" + imc);
-            System.out.println("Você é classificado como 'ACIMA DO PESO', com grau de obesidade I\n");
+            JOptionPane.showMessageDialog(null, "Você é classificado como 'ACIMA DO PESO', com grau de obesidade I",
+                    "Seu IMC é de: " + imc, JOptionPane.WARNING_MESSAGE);
+
         } else if (imc >= 30.0 && imc <= 39.9) {
-            System.out.println("Seu IMC é de:" + imc);
-            System.out.println("Você é classificado como 'OBESO', com grau de obesidade II\n");
+            JOptionPane.showMessageDialog(null, "Você é classificado como 'OBESO', com grau de obesidade II",
+                    "Seu IMC é de: " + imc, JOptionPane.WARNING_MESSAGE);
+
         } else if (imc >= 40.0) {
-            System.out.println("Seu IMC é de:" + imc);
-            System.out.println("Você é classificado como 'OBESO', com grau de obesidade III\n");
+            JOptionPane.showMessageDialog(null, "Você é classificado como 'OBESO', com grau de obesidade III",
+                    "Seu IMC é de: " + imc, JOptionPane.WARNING_MESSAGE);
         }
     }
 }
